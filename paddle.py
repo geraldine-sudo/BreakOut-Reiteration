0e416fd1a0a6779ff2932e008690fbc11bfde08c
geraldine-sudo <147319582+geraldine-sudo@users.noreply.github.com>
2024-12-21 18:50:31 +0800

    temp paddle

--- paddle.py
+++ paddle.py
@@ -10,16 +10,10 @@ class Paddle:
 
         self.w_paddle = 30
         self.h_paddle = 5
-
-        pyxel.init(self.w_layout, self.h_layout)
-        pyxel.run(self.update, self.draw)
     
     def update(self):
         self.x_paddle = min(self.w_layout - self.w_paddle, max(0, pyxel.mouse_x))
 
     def draw(self):
-        pyxel.cls(0)
         paddle = pyxel.rect(self.x_paddle, self.y_paddle, self.w_paddle, self.h_paddle, pyxel.COLOR_GREEN)
         cur_x = pyxel.text(5, 190, f'x: {self.x_paddle}', pyxel.COLOR_WHITE, None)
-
-Paddle()
