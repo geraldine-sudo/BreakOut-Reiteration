60fe2721b9ad81edef1102afe7349172ce54a5eb
geraldine-sudo <147319582+geraldine-sudo@users.noreply.github.com>
2024-12-21 18:49:59 +0800

    separate ball and paddle class

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import pyxel
 from paddle import Paddle
+from ball import Ball
 
 class Breakout:
     def __init__(self):
         self.w_layout = 120
         self.h_layout = 200
 
-        self.x_ball = 60
-        self.y_ball = 145
-
-        self.r_ball = 2
-
+        self.ball = Ball()
         self.paddle = Paddle()
 
         pyxel.init(self.w_layout, self.h_layout)
@@ -18,11 +15,11 @@ class Breakout:
 
     def update(self):
         self.paddle.update()
+        
 
     def draw(self):
         pyxel.cls(0)
         self.paddle.draw()
+        self.ball.draw()
         
-        ball = pyxel.circ(self.x_ball, self.y_ball, self.r_ball, pyxel.COLOR_DARK_BLUE)
-
 Breakout()
