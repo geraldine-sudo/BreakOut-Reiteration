aa5f7fc93ddaa93e25d1cb93bd4b72874532dce8
geraldine-sudo <147319582+geraldine-sudo@users.noreply.github.com>
2024-12-21 17:56:54 +0800

    modify layout specifications

--- layout.py
+++ layout.py
@@ -2,7 +2,16 @@ import pyxel
 
 class Breakout:
     def __init__(self):
-        pyxel.init(120, 180)
+        self.w_layout = 135
+        self.h_layout = 190
+
+        self.x_ball = 67.5
+        self.y_ball = 125
+
+        self.x_paddle = 52.5
+        self.y_paddle = 145
+
+        pyxel.init(self.w_layout, self.h_layout)
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -10,7 +19,7 @@ class Breakout:
 
     def draw(self):
         pyxel.cls(0)
-        ball = pyxel.circ(60, 110, 3, pyxel.COLOR_DARK_BLUE)
-        paddle = pyxel.rect(45, 130, 30, 6, pyxel.COLOR_GREEN)
+        ball = pyxel.circ(self.x_ball, self.y_ball, 2, pyxel.COLOR_DARK_BLUE)
+        paddle = pyxel.rect(self.x_paddle, self.y_paddle, 30, 5, pyxel.COLOR_GREEN)
 
 Breakout()
