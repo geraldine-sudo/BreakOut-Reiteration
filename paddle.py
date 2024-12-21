ee1e3fc9395fff259d817a5f83505d3c0c0483b4
geraldine-sudo <147319582+geraldine-sudo@users.noreply.github.com>
2024-12-21 19:40:51 +0800

    paddle hover

--- paddle.py
+++ paddle.py
@@ -6,14 +6,23 @@ class Paddle:
         self.h_layout = 200
 
         self.x_paddle = 45
-        self.y_paddle = 165
+        self.y_paddle = 160
 
         self.w_paddle = 30
         self.h_paddle = 5
-    
+
+        self.dx = 2
+
     def update(self):
-        self.x_paddle = min(self.w_layout - self.w_paddle, max(0, pyxel.mouse_x))
+        # always ensure that the mouse is within layout before moving
+        if pyxel.mouse_x > (self.w_layout/2) and (0 < pyxel.mouse_x <= self.w_layout):
+            self.x_paddle += self.dx
+        elif pyxel.mouse_x < (self.w_layout/2) and (0 < pyxel.mouse_x <= self.w_layout):
+            self.x_paddle -= self.dx
+
+        # horizontal border
+        self.x_paddle = max(0, min(self.x_paddle, pyxel.width - self.w_paddle))
 
     def draw(self):
-        paddle = pyxel.rect(self.x_paddle, self.y_paddle, self.w_paddle, self.h_paddle, pyxel.COLOR_GREEN)
-        cur_x = pyxel.text(5, 190, f'x: {self.x_paddle}', pyxel.COLOR_WHITE, None)
+        pyxel.rect(self.x_paddle, self.y_paddle, self.w_paddle, self.h_paddle, pyxel.COLOR_GREEN)
+        pyxel.text(5, 190, f'x: {self.x_paddle}', pyxel.COLOR_WHITE, None)
