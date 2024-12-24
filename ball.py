108ae27a705d790acc0cbb5307643f53b6663dfe
Katrina De Vera <159391790+Kmdevera@users.noreply.github.com>
2024-12-25 02:52:48 +0800

    ball vertical movement.py

--- ball.py
+++ ball.py
@@ -3,125 +3,125 @@ import math
 import pyxel
 class Ball:
     def __init__(self, x: float, y: float, w_pad: float) -> None:
+        #layout
         self.w_layout = 120
         self.h_layout = 200
-        self.G = 1
+        self.G = 5
+        self.launch = False
+
+        #paddle properties
         self.w_pad = w_pad
         self.x_pad = 0
-        self.acceleration = 0
-        self.angle = math.pi/36
-        self.v_pad = -math.sqrt(2*self.G*(y+3))/ math.sin(math.pi/36)
+        self.y_pad = y + 2
+        self.MR = 10
+        self.ML = 170
+        self.start_acc = -300
+
+        #ball properties
+        self.acc_y = 0
+        self.acc_x = 0
         self.vx = 0
         self.vy = 0
         self.v = 0
         self.t = 0
-        self.pad_y = y
-        self.degree: int| None = None
+        self.degree: int| None #angle in degrees
+        self.angle = 0 # angle in radians
 
+        # Main Ball Properties
         self.x_ball = x
-        self.y_ball = y
-
+        self.y_ball: float= y
         self.r_ball = 2
+        self.start_yloc = y
 
-        self.launch = False
     
     def update(self, x_pad: float):
 
         self.x_pad = x_pad
 
+        def ball_in_horbounds_of_paddle(x:float) -> bool:
+
+            return x_pad <= x+self.r_ball <= (x_pad+ self.w_pad) or  x_pad <= x-self.r_ball <= (x_pad+ self.w_pad)
+
+        def update_angle():
+            if ball_in_horbounds_of_paddle(self.x_ball):
+                    center_pad = x_pad + (self.w_pad/2)
+
+                    if self.x_ball == center_pad:
+                        self.degree = 90
+
+                    elif self.x_ball <= x_pad:
+                        self.degree = self.ML
+                    elif self.x_ball >= x_pad + self.w_pad:
+                        self.degree = self.MR
+
+                    else:
+                        self.degree = int(90 -(((90 -self.MR)/(self.w_pad/2))*(self.x_ball - center_pad)))
+
+
+                    self.angle = math.radians(self.degree)
+            else:   
+                self.degree = None
+
 
         if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
             self.launch = True
             self.degree = None
+            if ball_in_horbounds_of_paddle(self.x_ball):
+                self.vy = self.start_acc*math.sin(self.angle)
+                self.vx = self.start_acc*math.cos(self.angle)
+                self.acc_y = self.G*(-1)
+                self.acc_x = 0
+ 
 
-        if self.launch == True:
+        if self.launch:
+            
+            new_y_ball = self.y_ball + self.vy*(1/60) + 0.5*(self.acc_y + self.G)*(1/60)
 
-            if self.vy < 0:  # Moving upward
-                self.acceleration = -0.3 * self.G  # Counteract gravity a bit (reduced gravity)
-            else:  # Moving downward or at peak
-                self.acceleration = 0 
+            if ball_in_horbounds_of_paddle(self.x_ball):
 
-            self.new_y_ball = self.y_ball + (self.vy *self.t) + ((self.G + self.acceleration) * (self.t**2))
-            self.new_x_ball = self.x_ball + self.vx*(1/60)
+                if self.y_ball == self.y_pad -2: # on top of paddle
+                    self.y_ball = new_y_ball
 
-            if self.new_x_ball - self.r_ball -1 <= 0:
+                elif self.y_ball < self.start_yloc and new_y_ball > self.start_yloc:
+                    # from above the paddle the new calculated ball surpassed the paddle
 
-                self.x_ball = self.r_ball + 1
-                self.vx *=(-1)
+                    self.y_ball = self.start_yloc
+                    self.acc_y = self.G*(-1)
+                    self.vy = self.start_acc*math.sin(self.angle)
 
-            elif self.new_x_ball >= self.w_layout:
-                self.x_ball= self.w_layout - self.r_ball -1
-                self.vx *= (-1)
-
-            else:
-                self.x_ball = self.new_x_ball
-
-
-            if self.y_ball <= self.pad_y: # for y component
-
-                # Collision with the paddle
-                if ((x_pad <= self.x_ball +self.r_ball <= x_pad+ self.w_pad) or (x_pad <= self.x_ball - self.r_ball <= x_pad + self.w_pad))  and (self.new_y_ball +self.r_ball) > self.pad_y+ 1:
-                    self.y_ball= self.pad_y + 1
-                    self.vy = 3 + self.v_pad*math.sin(self.angle) # Invert velocity for an upward bounce
-                    self.t = 1/30
-
-                    if self.vx == 0:
-                        print(True)
-                        self.vx = -self.v_pad*math.cos(self.angle)
-                    elif self.vx > 0:  # Ball is coming from the left side
-                        self.vx = abs(self.vx)  # Change direction to the right
-                    else:  # Ball is coming from the right side
-                        self.vx = -abs(self.vx)  # Change direction to the left
-
-
-                else:
-                    if self.new_y_ball >= self.h_layout:
-                        self.y_ball = self.h_layout -self.r_ball -1
-                    else:
-                        if self.vy <= 0 and self.t % 0.1 == 0:
-                            self.acceleration -= 0.5
-
-                        self.y_ball = self.new_y_ball 
-                        self.vy = self.vy + (self.G +self.acceleration)*self.t
-                        self.t += 1/60
-
-                #    with the ceiling
-                if self.y_ball - self.r_ball <= 0:
+                elif self.y_ball < self.start_yloc and (new_y_ball- self.r_ball) <=0:
+                    # from below the paddle and new calculated ball surpassed the height layout
                     self.y_ball = self.r_ball
-                    self.vy =  3 # Invert velocity for a downward bounce
-                    self.t = 1/30
-
-            else:
-                if self.new_y_ball >= self.h_layout:
-                        self.y_ball = self.h_layout -self.r_ball -1
+                    self.acc_y = 0    #tentative
+                    self.vy =  (-1)*self.vy +(self.acc_y + self.G)*(1/60)
 
                 else:
-                    self.y_ball = self.new_y_ball 
+                    if new_y_ball- self.r_ball >= self.h_layout:
+                        self.y_ball = self.h_layout - self.r_ball
+                        self.vy = 0  
+                        self.acc_y = 0
 
-                    # Update the ball's velocity due to gravity
-                    self.vy = self.vy + self.G*self.t
-                    self.t += 1/60
-                    self.acceleration = 0.5
+                    else:
+                        self.y_ball = new_y_ball 
+                        self.vy += (self.acc_y + self.G)
+            else:
+
+                if (new_y_ball- self.r_ball) <=0:
+                    self.y_ball = self.r_ball
+                    self.acc_y = 0    #tentative
+                    self.vy = (-1)*self.vy + (self.acc_y + self.G)
+                
+                elif new_y_ball- self.r_ball >= self.h_layout:
+                    self.y_ball = self.h_layout - self.r_ball
+                    self.vy = 0
+                    self.acc_y = 0
+
+                else:
+                    self.y_ball = new_y_ball 
+                    self.vy += (self.acc_y + self.G)
 
         else:
-            if ((x_pad <= self.x_ball +self.r_ball <= x_pad+ self.w_pad) or (x_pad <= self.x_ball - self.r_ball <= x_pad + self.w_pad)):
-                center_pad = x_pad + (self.w_pad/2)
-
-                if self.x_ball == center_pad:
-                    self.degree = 90
-
-                elif self.x_ball <= x_pad:
-                    self.degree = 175
-                elif self.x_ball >= x_pad + self.w_pad:
-                    self.degree = 5
-
-                else:
-                    self.degree = int(90 -((85/15)*(self.x_ball - center_pad)))
-
-
-                self.angle = math.radians(self.degree)
-            else:   
-                self.degree = None
+            update_angle()
 
     def draw(self):
 
