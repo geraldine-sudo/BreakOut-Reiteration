ed783a159239a0ed90b71583cf9c687eff822fc2
Katrina De Vera <159391790+Kmdevera@users.noreply.github.com>
2024-12-25 04:19:05 +0800

    Update ball horizontal movement.py

--- ball.py
+++ ball.py
@@ -32,43 +32,62 @@ class Ball:
         self.y_ball: float= y
         self.r_ball = 2
         self.start_yloc = y
+        print(self.y_pad -2)
 
+    def ball_in_horbounds_of_paddle(self, x:float) -> bool:
+
+            return self.x_pad <= x+self.r_ball <= (self.x_pad+ self.w_pad
+            ) or  self.x_pad <= x-self.r_ball <= (self.x_pad+ self.w_pad)
     
-    def update(self, x_pad: float):
-
-        self.x_pad = x_pad
-
-        def ball_in_horbounds_of_paddle(x:float) -> bool:
-
-            return x_pad <= x+self.r_ball <= (x_pad+ self.w_pad) or  x_pad <= x-self.r_ball <= (x_pad+ self.w_pad)
-
-        def update_angle():
-            if ball_in_horbounds_of_paddle(self.x_ball):
-                    center_pad = x_pad + (self.w_pad/2)
+    def update_angle(self):
+            if self.ball_in_horbounds_of_paddle(self.x_ball):
+                    center_pad = self.x_pad + (self.w_pad/2)
 
                     if self.x_ball == center_pad:
                         self.degree = 90
 
-                    elif self.x_ball <= x_pad:
+                    elif self.x_ball <= self.x_pad:
                         self.degree = self.ML
-                    elif self.x_ball >= x_pad + self.w_pad:
+                    elif self.x_ball >= self.x_pad + self.w_pad:
                         self.degree = self.MR
 
                     else:
                         self.degree = int(90 -(((90 -self.MR)/(self.w_pad/2))*(self.x_ball - center_pad)))
-
 
                     self.angle = math.radians(self.degree)
             else:   
                 self.degree = None
 
 
+    def line_interpolation(self, x: float, y:float, find:str, coor:float)-> float:
+        "coor is your new set coordinate"
+
+        if (x - self.x_ball) != 0 and (y - self.y_ball) != 0 :
+            m = (y - self.y_ball)/(x - self.x_ball)
+            b = self.y_ball - m*self.x_ball
+
+            if find == "x":
+                return (coor + b)/m
+            
+            else:
+                return m*coor + b
+            
+        else:
+            if find == "x":
+                return self.x_ball
+            else:
+                return self.y_ball
+    
+    def update(self, x_pad: float):
+
+        self.x_pad = x_pad
+
         if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
             self.launch = True
             self.degree = None
-            if ball_in_horbounds_of_paddle(self.x_ball):
+            if self.ball_in_horbounds_of_paddle(self.x_ball):
                 self.vy = self.start_acc*math.sin(self.angle)
-                self.vx = self.start_acc*math.cos(self.angle)
+                self.vx = -self.start_acc*math.cos(self.angle)
                 self.acc_y = self.G*(-1)
                 self.acc_x = 0
  
@@ -76,8 +95,24 @@ class Ball:
         if self.launch:
             
             new_y_ball = self.y_ball + self.vy*(1/60) + 0.5*(self.acc_y + self.G)*(1/60)
+            new_x_ball = self.x_ball + self.vx*(1/60) + 0.5*(self.acc_x + self.G)*(1/60)
 
-            if ball_in_horbounds_of_paddle(self.x_ball):
+            if new_x_ball - self.r_ball <= 0:
+                self.vx*=(-1)
+                self.acc_x*=(-1)
+                self.x_ball = self.r_ball
+                new_y_ball = self.line_interpolation(new_x_ball,new_y_ball,"y", self.x_ball)
+
+            elif new_x_ball +self.r_ball >= self.w_layout:
+                self.vx*=(-1)
+                self.acc_x*=(-1)
+                self.x_ball = self.w_layout -2
+                new_y_ball = self.line_interpolation(new_x_ball,new_y_ball,"y", self.x_ball)
+            else:
+                self.x_ball = new_x_ball
+
+
+            if self.ball_in_horbounds_of_paddle(self.x_ball):
 
                 if self.y_ball == self.y_pad -2: # on top of paddle
                     self.y_ball = new_y_ball
@@ -89,11 +124,14 @@ class Ball:
                     self.acc_y = self.G*(-1)
                     self.vy = self.start_acc*math.sin(self.angle)
 
-                elif self.y_ball < self.start_yloc and (new_y_ball- self.r_ball) <=0:
-                    # from below the paddle and new calculated ball surpassed the height layout
+                elif (new_y_ball- self.r_ball) <=0:
+                    #new calculated y is out of frame
+
                     self.y_ball = self.r_ball
                     self.acc_y = 0    #tentative
                     self.vy =  (-1)*self.vy +(self.acc_y + self.G)*(1/60)
+
+                    self.x_ball = self.line_interpolation(self.x_ball,new_y_ball,"x", self.y_ball)
 
                 else:
                     if new_y_ball- self.r_ball >= self.h_layout:
@@ -107,9 +145,11 @@ class Ball:
             else:
 
                 if (new_y_ball- self.r_ball) <=0:
+                    #new calculated y is out of frame
                     self.y_ball = self.r_ball
                     self.acc_y = 0    #tentative
                     self.vy = (-1)*self.vy + (self.acc_y + self.G)
+                    self.x_ball = self.line_interpolation(self.x_ball,new_y_ball,"x", self.y_ball)
                 
                 elif new_y_ball- self.r_ball >= self.h_layout:
                     self.y_ball = self.h_layout - self.r_ball
@@ -121,7 +161,7 @@ class Ball:
                     self.vy += (self.acc_y + self.G)
 
         else:
-            update_angle()
+            self.update_angle()
 
     def draw(self):
 
