7630561cd1381076e2b34c1f35662a0f69279248
Katrina De Vera <159391790+Kmdevera@users.noreply.github.com>
2024-12-27 01:59:23 +0800

    paddle start state.py

--- paddle.py
+++ paddle.py
@@ -6,24 +6,31 @@ class Paddle:
         self.w_layout = 120
         self.h_layout = 200
 
-        self.x_paddle = 45
-        self.y_paddle = 160
-
         self.w_paddle = 22
         self.h_paddle = 10
 
+        self.x_paddle = self.w_layout//2 -self.w_paddle//2
+        self.y_paddle = 160
+
         self.dx = 3
+        self.launch = False
 
     def update(self):
-        # always ensure that the mouse is within layout before moving
-        if pyxel.mouse_x > self.w_paddle / 2 + self.x_paddle and (0 < pyxel.mouse_y <= self.h_layout):
-            self.x_paddle += self.dx
 
-        if pyxel.mouse_x < self.w_paddle / 2 + self.x_paddle and (0 < pyxel.mouse_y <= self.h_layout):
-            self.x_paddle -= self.dx
+        if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
+            self.launch = True
 
-        # horizontal border
-        self.x_paddle = max(0, min(self.x_paddle, pyxel.width - self.w_paddle))
+        if self.launch:
+
+            # always ensure that the mouse is within layout before moving
+            if pyxel.mouse_x > self.w_paddle / 2 + self.x_paddle and (0 < pyxel.mouse_y <= self.h_layout):
+                self.x_paddle += self.dx
+    
+            if pyxel.mouse_x < self.w_paddle / 2 + self.x_paddle and (0 < pyxel.mouse_y <= self.h_layout):
+                self.x_paddle -= self.dx
+
+            # horizontal border
+            self.x_paddle = max(0, min(self.x_paddle, pyxel.width - self.w_paddle))
 
     def draw(self):
         pyxel.blt(self.x_paddle, self.y_paddle, 0, 0, 136, self.w_paddle, self.h_paddle, 0)
