42dc8b31a699f8f2c75901307b789418a8803dbd
Katrina De Vera <159391790+Kmdevera@users.noreply.github.com>
2024-12-31 23:48:32 +0800

    randomizer for power ups.py

--- score_object.py
+++ score_object.py
@@ -1,18 +1,28 @@
 import pyxel
 from paddle import Paddle
+import random
+
+def randomize_choice( X: int)-> int:
+        choices = [1, 2, 3, 4, 5] # to be updated based on json 
+        weights= [(100 - X)*0.01, X* 0.0025,  X* 0.0025,  X* 0.0025,  X* 0.0025]  
+        # Probabilities add up to 1 divides X to 4 powerups the rest will be normal score
+        return random.choices(choices, weights, k=1)[0] 
 
 class Score_Object:
 
-    def __init__(self, x: int, y: int, points: int, paddle: Paddle, gravity: int) -> None:
+    def __init__(self, x: int, y: int, points: int, paddle: Paddle, gravity: int, X: int) -> None:
+        self.X = X
         self.x_obj = x
         self.y_obj = y
+        self.type = randomize_choice(self.X) 
         self.points = points
         self.paddle = paddle
         self.acquired = False
         self.alive = True
         self.G = gravity
         self.h_obj = 3  
-        self.w_obj = 3  
+        self.w_obj = 3 
+
 
     def update(self):
         if self.alive:
@@ -31,6 +41,19 @@ class Score_Object:
                 # If the object hasn't hit the paddle yet, keep falling
                 self.y_obj = new_y
 
-    def draw(self):
-        if self.alive:  
-            pyxel.rect(self.x_obj, self.y_obj, self.w_obj, self.h_obj, pyxel.COLOR_LIME)
+    def draw(self): # testing only
+        if self.alive: 
+            if self.type == 1: 
+                pyxel.rect(self.x_obj, self.y_obj, self.w_obj, self.h_obj, pyxel.COLOR_LIME)
+            elif self.type == 2:
+                pyxel.rect(self.x_obj, self.y_obj, self.w_obj, self.h_obj, pyxel.COLOR_DARK_BLUE)
+            elif self.type == 3:
+                pyxel.rect(self.x_obj, self.y_obj, self.w_obj, self.h_obj, pyxel.COLOR_PEACH)
+            elif self.type == 4:
+                pyxel.rect(self.x_obj, self.y_obj, self.w_obj, self.h_obj, pyxel.COLOR_PINK)
+
+            else:
+                pyxel.rect(self.x_obj, self.y_obj, self.w_obj, self.h_obj, pyxel.COLOR_GRAY)
+
+
+            
