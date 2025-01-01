5b0bde80b5ebc3982896ff52fbb51a845a3e0ace
Katrina De Vera <159391790+Kmdevera@users.noreply.github.com>
2025-01-02 01:40:43 +0800

    streak.py

--- paddle.py
+++ paddle.py
@@ -7,7 +7,7 @@ class Paddle:
         self.h_layout = 200
 
         self.w_paddle = 22
-        self.h_paddle = 10
+        self.h_paddle = 14
 
         self.x_paddle = self.w_layout//2 -self.w_paddle//2
         self.y_paddle = 160
@@ -31,8 +31,8 @@ class Paddle:
             self.x_paddle = max(0, min(self.x_paddle, pyxel.width - self.w_paddle))
 
     def draw(self):
-        pyxel.blt(self.x_paddle, self.y_paddle, 0, 0, 136, self.w_paddle, self.h_paddle, 0)
+        pyxel.blt(self.x_paddle, self.y_paddle, 0, 0, 135, self.w_paddle, self.h_paddle, 0)
         # pyxel.blt(self.x_paddle, self.y_paddle, 0, 24, 136, self.w_paddle, self.h_paddle, 0)
-        if (0 < pyxel.mouse_x <= self.w_layout) and (0 < pyxel.mouse_y <= self.h_layout):
-            pyxel.text(5, 185, f'x: {pyxel.mouse_x}', pyxel.COLOR_BLACK, None)
+        #if (0 < pyxel.mouse_x <= self.w_layout) and (0 < pyxel.mouse_y <= self.h_layout):
+            #pyxel.text(5, 185, f'x: {pyxel.mouse_x}', pyxel.COLOR_BLACK, None)
             # pyxel.text(5, 190, f'x: {pyxel.mouse_x}', pyxel.COLOR_WHITE, None)
