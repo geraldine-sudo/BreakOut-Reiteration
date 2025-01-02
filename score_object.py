68d3e2847fef40f7aa98f8e7e178ebd576a5c896
Katrina De Vera <159391790+Kmdevera@users.noreply.github.com>
2025-01-03 02:16:45 +0800

    gamble, gravity, powerup text.py

--- score_object.py
+++ score_object.py
@@ -3,194 +3,33 @@ from paddle import Paddle
 import random
 import json
 from pprint import pprint
-
-brick_specs = {1: {
-                    "score": 50,
-                    "hits": 1,
-                    "img": 0,
-                    "u": 80,
-                    "v": 64,
-                    "w": 16,
-                    "h": 16
-                    },
-        2: {        "score": 100,
-                    "hits": 2,
-                    "img": 0,
-                    "u": 112,
-                    "v": 64,
-                    "w": 16,
-                    "h": 16
-               },
-        2.1: {      "score": 100,
-                    "hits": 2,
-                    "img": 0,
-                    "u": 112,
-                    "v": 80,
-                    "w": 16,
-                    "h": 16
-               },
-        3: {        "score": 150,
-                    "hits": 3,
-                    "img": 0,
-                    "u": 96,
-                    "v": 48,
-                    "w": 16,
-                    "h": 16
-                    },
-        3.1: {        "score": 150,
-                    "hits": 3,
-                    "img": 0,
-                    "u": 96,
-                    "v": 64,
-                    "w": 16,
-                    "h": 16
-                    },
-        3.2: {        "score": 150,
-                    "hits": 3,
-                    "img": 0,
-                    "u": 112,
-                    "v": 48,
-                    "w": 16,
-                    "h": 16
-                    },
-        4: {        "score": 0,
-                    "hits": -1,
-                    "img": 0,
-                    "u": 80,
-                    "v": 80,
-                    "w": 16,
-                    "h": 16
-                    },
-        5: {        "score": 50,
-                    "hits": 1,
-                    "img": 0,
-                    "u": 80,
-                    "v": 48,
-                    "w": 16,
-                    "h": 16
-                    },
-        6: {     "score": 0,
-                    "hits": -1,
-                    "img": 0,
-                    "u": 96,
-                    "v": 80,
-                    "w": 16,
-                    "h": 16
-                    },
-        6.1: {     "score": 0,
-                    "hits": -1,
-                    "img": 0,
-                    "u": 112,
-                    "v": 96,
-                    "w": 16,
-                    "h": 16
-                    },
-        6.2: {     "score": 0,
-                    "hits": -1,
-                    "img": 0,
-                    "u": 152,
-                    "v": 80,
-                    "w": 7,
-                    "h": 7
-                    },
-        7: {        "score": 0,
-                    "hits": -1,
-                    "img": 0,
-                    "u": 0,
-                    "v": 120,
-                    "w": 16,
-                    "h": 16
-                    },
-        8: {        "score": 0,
-                    "hits": 1,
-                    "img": 0,
-                    "u": 144,
-                    "v": 32,
-                    "w": 7,
-                    "h": 7
-                    },
-        8.1: {       "score": 0,
-                    "hits": 1,
-                    "img": 0,
-                    "u": 152,
-                    "v": 32,
-                    "w": 7,
-                    "h": 7
-                    },
-        8.2: {        "score": 0,
-                    "hits": 1,
-                    "img": 0,
-                    "u": 144,
-                    "v": 40,
-                    "w": 7,
-                    "h": 7
-                    },
-        8.3: {        "score": 0,
-                    "hits": 1,
-                    "img": 0,
-                    "u": 152,
-                    "v": 40,
-                    "w": 7,
-                    "h": 7
-                    },
-        9: {        "score": 50,
-                    "hits": 1,
-                    "img": 0,
-                    "u": 128,
-                    "v": 80,
-                    "w": 7,
-                    "h": 7
-                    },
-        9.1: {        "score": 50,
-                    "hits": 1,
-                    "img": 0,
-                    "u": 136,
-                    "v": 80,
-                    "w": 7,
-                    "h": 7
-                    },
-        9.2: {        "score": 50,
-                    "hits": 1,
-                    "img": 0,
-                    "u": 128,
-                    "v": 88,
-                    "w": 7,
-                    "h": 7
-                    },
-        9.3: {        "score": 50,
-                    "hits": 1,
-                    "img": 0,
-                    "u": 136,
-                    "v": 88,
-                    "w": 7,
-                    "h": 7
-                    },
-        9.4: {        "score": 50,
-                    "hits": 1,
-                    "img": 0,
-                    "u": 144,
-                    "v": 80,
-                    "w": 7,
-                    "h": 7
-                    }
-}
+from bricks import access_json
 
 
-#store brick specifications in json file
-with open('bricks.json', 'w') as f:
-    json.dump(brick_specs, f)
+class Powerup_Text:
+    def __init__(self, txt_x: int, txt_y: int, txt: str, rec_x: int, rec_y: int, rec_w: int, rec_h: int, rec_color: int) -> None:
 
-def access_json(file: str):
-    brick = {}
+        self.txt_x = txt_x
+        self.txt_y = txt_y
+        self.txt = txt
+        self.rec_x = rec_x
+        self.rec_y = rec_y
+        self.rec_w = rec_w
+        self.rec_h = rec_h
+        self.rec_color = rec_color
+        self.t = 0
 
-    with open(file+'.json', 'r') as f:
-        brick = json.load(f)
+    def update(self):
+        self.t += 1
 
-    return brick
+    def draw(self):
+        pyxel.rect(self.rec_x,self.rec_y, self.rec_w, self.rec_h, self.rec_color)
+        pyxel.text(self.txt_x, self.txt_y, self.txt, pyxel.COLOR_WHITE, None)
+
 
 
 def randomize_choice( X: int)-> int:
-        choices = [1,2 , 3, 4, 5] # to be updated based on json 
+        choices = [1,2,3, 4, 5]
         weights= [(100 - X)*0.01, X* 0.0025,  X* 0.0025,  X* 0.0025,  X* 0.0025]  
         # Probabilities add up to 1 divides X to 4 powerups the rest will be normal score
         return random.choices(choices, weights, k=1)[0] 
@@ -228,7 +67,7 @@ class Streak_Score:
 
 class Score_Object:
 
-    def __init__(self, main, x: int, y: int, brick_level: str, points: int, paddle: Paddle, gravity: int, X: int) -> None:
+    def __init__(self, main, x: int, y: int, brick_level: str, paddle: Paddle, gravity: int, X: int) -> None:
         self.main = main
         self.X = X
         self.x_obj = x
@@ -236,11 +75,12 @@ class Score_Object:
         self.brick_level = brick_level
         self.type = randomize_choice(self.X) 
         self.image = image_ref(self.type,self.brick_level)
-        self.points = points
         self.paddle = paddle
         self.acquired = False
         self.alive = True
-        self.G = gravity  
+        self.G = gravity
+        self.vy = 20
+        self.acc_y = 0
 
         # Load brick data once
         self.obj_data = access_json('bricks')
@@ -254,25 +94,30 @@ class Score_Object:
 
     def powerup(self):
         if self.image == "9":
-            print(True, "add heart")
             self.main.lives += 1
+            self.main.powerup_text.append(Powerup_Text(40,35, "Life UP!", 33,33,45,9,8))
 
         elif self.image == "9.2":
-            print(True, "antigravity")
             self.main.anti_gravity = True
             self.main.t_anti_gravity += 300
+            self.main.powerup_text.append(Powerup_Text(33,35, "Anti-Gravity!", 30,33,55,9,3))
         elif self.image == "9.3":
             self.main.extend_paddle = True
             self.main.t_extend_paddle += 300
+            self.main.powerup_text.append(Powerup_Text(43,35, "EXTEND!", 38,33,35,9,13))
 
         else:
-            pass
+            self.main.powerup_text.append(Powerup_Text(35,35,  f"Better Luck\n Next Time!", 33,33,47,15,9))
 
 
 
     def update(self):
         if self.alive:
-            new_y = self.y_obj + self.G  # Apply gravity
+            if self.main.anti_gravity:
+                self.acc_y = -self.G
+            else:
+                self.acc_y = 0
+            new_y = self.y_obj + self.vy * (1 / 60) + 0.5 * (self.G) * (1 / 60)  # Apply gravity
 
             if self.acquired:
                 self.alive = False
@@ -285,8 +130,10 @@ class Score_Object:
                 
                 self.y_obj = self.paddle.y_paddle - self.h_obj
                 self.acquired = True  
-                self.powerup()
-
+                if self.type != 1:
+                    if self.type == 5:
+                        self.image = random.choice(["9", "9.1", "9.2", "9.3"])
+                    self.powerup()
 
             elif (self.paddle.x_paddle - self.w_obj <self.x_obj < self.paddle.x_paddle + self.paddle.w_paddle and
                   self.paddle.y_paddle - self.h_obj<= self.y_obj < self.paddle.y_paddle + self.paddle.h_paddle):
@@ -294,13 +141,20 @@ class Score_Object:
                 # if obj inside the paddle will not be drawn anymore 
                 self.acquired = True
                 self.alive = False
-                self.powerup()
+                if self.type != 1:
+                    if self.type == 5:
+                        self.image = random.choice(["9", "9.1", "9.2", "9.3"])
+                    self.powerup()
 
             elif self.y_obj >= self.paddle.h_layout:
                 self.alive = False
             else:
                 # If the object hasn't hit the paddle yet, keep falling
                 self.y_obj = new_y
+
+            
+
+            self.vy += (self.acc_y +self.G)
     def draw(self): # testing only
         if self.alive: 
             pyxel.blt(self.x_obj, self.y_obj, self.img,self.u,self.v, self.w_obj,self.h_obj, 0)
